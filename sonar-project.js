092d0a23b6d2c11602c14be8843c98b60a5f170e
naveenkumarreddy13 <139735589+naveenkumarreddy13@users.noreply.github.com>
2023-08-25 15:18:49 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://65.2.5.251:9000/',
+  serverUrl: 'http://13.126.236.40:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'squ_c38245de6343beea3b9a299702af209e39861208',
+	    'sonar.login': 'squ_11c8e2d6a3d7f7fd2492f076ac4aa7599ff9e033',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
